8984a645a8a560cee1e2af3810f5cbba4f689281
doory <78720364+dooory@users.noreply.github.com>
2025-06-24 01:38:40 +1200

    Added lazydev blink.cmp integrations

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,6 +1,7 @@
 return {
 	'saghen/blink.cmp',
-	-- optional: provides snippets for the snippet source
+	event = 'VimEnter',
+	version = '1.*',
 	dependencies = {
 		{
 			'L3MON4D3/LuaSnip',
@@ -10,9 +11,8 @@ return {
 				require('luasnip.loaders.from_vscode').lazy_load()
 			end
 		},
+		'folke/lazydev.nvim',
 	},
-
-	version = '1.*',
 
 	opts = {
 		-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
@@ -35,12 +35,16 @@ return {
 
 		-- (Default) Only show the documentation popup when manually triggered
 		completion = { documentation = { auto_show = true } },
-
 		-- Default list of enabled providers defined so that you can extend it
 		-- elsewhere in your config, without redefining it, due to `opts_extend`
 		sources = {
-			default = { 'lsp', 'path', 'snippets', 'buffer' },
+			default = { 'lsp', 'path', 'snippets', 'buffer', 'lazydev' },
+
+			providers = {
+				lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+			},
 		},
+
 		snippets = { preset = 'luasnip' },
 
 		-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
@@ -48,7 +52,7 @@ return {
 		-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
 		--
 		-- See the fuzzy documentation for more information
-		fuzzy = { implementation = "prefer_rust_with_warning" }
+		fuzzy = { implementation = "lua" }
 	},
 	opts_extend = { "sources.default" }
 }
